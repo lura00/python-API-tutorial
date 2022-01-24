dc03e542a0f17cd2e86378e813ddf09374458112
lura00 <ludvig.skinstad@gmail.com>
2022-01-24 20:44:26 +0100

    Added CORS-middleware and more columns to postgres from alembic

--- app/main.py
+++ app/main.py
@@ -3,10 +3,23 @@ from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
+# Allows people from other domains talk to my API
+from fastapi.middleware.cors import CORSMiddleware
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# A function that goes through the request.
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include our new folder router and the files in it. Then goes to the files and see if it can match router.
 
