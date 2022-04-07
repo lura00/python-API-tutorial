9064828e3a9f5669486e8c361371be0b6ad811fc
lura00 <ludvig.skinstad@gmail.com>
2022-04-07 20:53:55 +0200

    new test, creating personilized greeting front page

--- app/main.py
+++ app/main.py
@@ -34,5 +34,5 @@ app.include_router(vote.router)
 @app.get("/")
 def root(request: Request):
     ip = request.client.host
-    host = socket.gethostbyaddr(ip)
-    return {"message": f"Hello {host[0]}, pleased that you find yourself in here. type /docs in the url and test out my twitter-like program in fastapi/Swagger"}
+    host = socket.gethostname()
+    return {"message": f"Hello {host} from {ip}, pleased that you find yourself in here. type /docs in the url and test out my twitter-like program in fastapi/Swagger"}
